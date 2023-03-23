aa575f3a38d82a15319d61ef1f208dbccc33ce3e
seita1996 <taha.seita@gmail.com>
2023-03-24 08:18:40 +0900

    Shortened command method names

--- lib/jott/cli.rb
+++ lib/jott/cli.rb
@@ -4,26 +4,27 @@ require_relative 'memo'
 
 class CLI < Thor
   desc "clear", "clear all memos"
+  method_option :aliases => "-cl"
   def clear
     Memo.new.clear
     puts "Clear all memos".colorize(:green)
   end
 
-  desc "create", "create a new memo"
-  def create(text)
+  desc "add", "Add a new memo"
+  def add(text)
     title = text[0, 30]
     Memo.new.create(title:, body: text)
-    puts "Created new memo: #{title}".colorize(:green)
+    puts "Added new memo: #{title}".colorize(:green)
   end
 
   desc "delete", "delete memo ID"
-  def delete(id)
+  def rm(id)
     Memo.new.delete(id:)
     puts "Deleted the memo: #{id}".colorize(:green)
   end
 
   desc "list", "list all memos"
-  def list
+  def ls
     memos = Memo.new.all
     memos.each do |memo|
       puts "#{memo[0]}. #{memo[1]}: #{memo[2]}"
--- spec/jott/cli_spec.rb
+++ spec/jott/cli_spec.rb
@@ -25,32 +25,32 @@ RSpec.describe CLI do
     end
   end
 
-  describe "#create" do
+  describe "#add" do
     context "when command executed by over 30 chars args" do
       let(:text) { 'There is always light behind the clouds.' }
 
-      it "created message and the first 30 chars of the memo created are displayed in green." do
+      it "added message and the first 30 chars of the memo added are displayed in green." do
         first_30_chars = 'There is always light behind t'
         expect {
-          CLI.start(['create', text])
-        }.to output("Created new memo: #{first_30_chars}".colorize(:green).concat("\n")).to_stdout
+          CLI.start(['add', text])
+        }.to output("Added new memo: #{first_30_chars}".colorize(:green).concat("\n")).to_stdout
       end
 
-      it "creates a new memo" do
+      it "adds a new memo" do
         init_count = Memo.new.count[0][0]
-        CLI.start(['create', text])
+        CLI.start(['add', text])
         final_count = Memo.new.count[0][0]
         expect(final_count).to eq init_count + 1
       end
 
       it "the title of the new memo is the first 30 chars of the memo." do
         first_30_chars = 'There is always light behind t'
-        CLI.start(['create', text])
+        CLI.start(['add', text])
         expect(Memo.new.last[0][2]).to eq first_30_chars
       end
 
       it "the body of the new memo is the text entered." do
-        CLI.start(['create', text])
+        CLI.start(['add', text])
         expect(Memo.new.last[0][3]).to eq text
       end
     end
@@ -58,26 +58,26 @@ RSpec.describe CLI do
     context "when command executed by under 30 chars args" do
       let(:text) { 'Hi Bob.' }
 
-      it "created message and the memo created are displayed in green." do
+      it "added message and the memo added are displayed in green." do
         expect {
-          CLI.start(['create', text])
-        }.to output("Created new memo: #{text}".colorize(:green).concat("\n")).to_stdout
+          CLI.start(['add', text])
+        }.to output("Added new memo: #{text}".colorize(:green).concat("\n")).to_stdout
       end
 
-      it "creates a new memo" do
+      it "adds a new memo" do
         init_count = Memo.new.count[0][0]
-        CLI.start(['create', text])
+        CLI.start(['add', text])
         final_count = Memo.new.count[0][0]
         expect(final_count).to eq init_count + 1
       end
 
       it "the title of the new memo is the text entered." do
-        CLI.start(['create', text])
+        CLI.start(['add', text])
         expect(Memo.new.last[0][2]).to eq text
       end
 
       it "the body of the new memo is the text entered." do
-        CLI.start(['create', text])
+        CLI.start(['add', text])
         expect(Memo.new.last[0][3]).to eq text
       end
     end
@@ -85,21 +85,21 @@ RSpec.describe CLI do
     # TODO: Add tests for anomalous systems
   end
 
-  describe "#delete" do
+  describe "#rm" do
     context "when an existing id is passed as an arg" do
       let(:memo) { Memo.new.create(title: 'test', body: 'test') }
       let(:id) { Memo.new.last[0][0] }
 
       it "deleted message and the id are displayed in green." do
         expect {
-          CLI.start(['delete', id])
+          CLI.start(['rm', id])
         }.to output("Deleted the memo: #{id}".colorize(:green).concat("\n")).to_stdout
       end
 
       it "deletes the memo" do
         memo
         init_count = Memo.new.count[0][0]
-        CLI.start(['delete', id])
+        CLI.start(['rm', id])
         final_count = Memo.new.count[0][0]
         expect(final_count).to eq init_count - 1
       end
@@ -108,7 +108,7 @@ RSpec.describe CLI do
     # TODO: Add tests for anomalous systems
   end
 
-  describe "#list" do
+  describe "#ls" do
     context "when executed" do
       let(:memo1) { Memo.new.create(title: 'test1', body: 'test1') }
       let(:memo2) { Memo.new.create(title: 'test2', body: 'test2') }
@@ -118,7 +118,7 @@ RSpec.describe CLI do
         memo1
         memo2
         expect {
-          CLI.start(['list'])
+          CLI.start(['ls'])
         }.to output("1. test1: test1\n2. test2: test2\n").to_stdout
       end
     end
